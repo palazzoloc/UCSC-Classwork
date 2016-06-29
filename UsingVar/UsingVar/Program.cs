b248b704b27e7db68458ce8e9d13363c61f23638
palazzoloc <palazzoloc@outlook.com>
2016-06-29 14:18:35 -0700

    Add files via upload

---
+++ UsingVar/UsingVar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arguments: name, age
+            Console.WriteLine("You entered the following {0} command line arguments: ", args.Length);
+            for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine("Argument[{0}] = [{1}]", i, args[i]);
+
+                }
+            Console.Read();                            
+        }
+    }
+}
